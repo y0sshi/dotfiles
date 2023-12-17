369a803a5b64d3e4009101ba3251fc9138c278d5
Naofumi Yoshinaga <y0sshi316n@gmail.com>
2023-12-17 12:06:22 +0900

    modify bash settings

---
+++ user.bash
@@ -0,0 +1,22 @@
+# Prompt
+PS1="\[\e[1;4;32m\]\u@\h:\[\e[m\]\[\e[1;33m\]\w\[\e[m\] \[\e[1;36m\][\D{%Y/%m/%d(%a)}]\[\e[m\]\n "
+
+# alias
+alias x='exit'
+alias ls='ls -G --color'
+alias l='ls'
+alias s='ls'
+alias la='ls -a'
+alias ll='ls -l'
+alias lal='ls -al'
+alias lla='ls -la'
+
+# Path
+export PATH=${HOME}/.cargo/bin:${PATH}
+
+# Editor
+export EDITOR=nvim
+
+# github access token
+export CR_PAT_GITHUB=
+
